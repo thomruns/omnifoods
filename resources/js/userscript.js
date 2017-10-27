e8ca32cf9ead6d599d69c6020974fe885827617c
thomruns <thomdavid74@gmail.com>
2017-10-27 15:00:44 -0700

    Animated features, phone, cities, CTA

--- resources/js/userscript.js
+++ resources/js/userscript.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
       }
   },
     {
-      offset: '10%;'
+      offset: '10%'
     }); //end waypoints sticky nav display
 
   // Scroll on buttons******************** //
@@ -61,5 +61,32 @@ $(document).ready(function(){
       }
     });
     // ******** Navigation scroll end *************
+
+    // ********* Animations on scroll ***************
+    $('.js-wp-1').waypoint(function(direction){
+      $('.js-wp-1').addClass('animated fadeIn');
+    }, {
+      offset: '50%'
+    });
+
+    $('.js-wp-2').waypoint(function(direction){
+      $('.js-wp-2').addClass('animated fadeInUp');
+    }, {
+      offset: '65%'
+    });
+
+    $('.js-wp-3').waypoint(function(direction){
+      $('.js-wp-3').addClass('animated fadeIn');
+    }, {
+      offset: '60%'
+    });
+
+    $('.js-wp-4').waypoint(function(direction){
+      $('.js-wp-4').addClass('animated pulse');
+    }, {
+      offset: '50%'
+    });
+
+
 
 }); // end document.ready function //
