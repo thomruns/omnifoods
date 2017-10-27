3de899423b8d9c54277acb0104ef62f26aecb05f
thomruns <thomdavid74@gmail.com>
2017-10-27 11:17:27 -0700

    Sticky navigation coded

--- resources/js/userscript.js
+++ resources/js/userscript.js
@@ -1,5 +1,24 @@
 //This is file of custom scripts for the Omnifood website.
 $(document).ready(function(){
+     $('.js--section-features').waypoint(function(direction){
+       if (direction =='down') {
+           $('nav').addClass('sticky');
+         } else {
+           $('nav').removeClass('sticky');
+         }
+     },
+       {
+         offset: '10%;'
+       });
+
+/*
+var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window')
+}, {
+  offset: '25%'
+})
+
+*/
 
 
-});
+}); /*end document.ready function*/
